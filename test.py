4aed80cc5978e595804d086cb52d068a967876dd
Blessingedem98 <edemblessing33@gmail.com>
2022-06-03 12:21:32 +0100

    added code to open csv file

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-123
+import csv
+file = open("movieprojectcsv.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()
